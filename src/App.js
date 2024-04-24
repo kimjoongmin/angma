ab7914e6703a8338288ce4998487bd65e847eace
jjoongmin <jjoongmin@gmail.com>
2024-04-24 11:09:47 +0900

    ㅁ

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@
 // import EmptyPage from "./components/EmptyPage";
 // //Switch 대신 Routes
 // //Route에서 component prop 대신 element prop 사용하여 jsx 요소 전달
-// export default function App() {
+// export default function App() {//
 //     return (
 //         <div className="App">
 //             <BrowserRouter>
