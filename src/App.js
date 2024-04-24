1ff45c7ae059c764ae9850e680004b849139db64
jjoongmin <jjoongmin@gmail.com>
2024-04-24 10:36:07 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React from 'react'
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
-import Day from './components/Day'
-import DayList from './components/DayList'
-import EmptyPage from './components/EmptyPage'
-import Headers from './components/Header'
+// import React from "react";
+// import Header from "./components/Header";
+// import DayList from "./components/DayList";
+// import Day from "./components/Day";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import EmptyPage from "./components/EmptyPage";
+// //Switch 대신 Routes
+// //Route에서 component prop 대신 element prop 사용하여 jsx 요소 전달
+// export default function App() {
+//     return (
+//         <div className="App">
+//             <BrowserRouter>
+//                 <Header />
+//                 <Routes>
+//                     <Route path="/" element={<DayList />} />
+//                     <Route path="/day/:day" element={<Day />} />
+//                     <Route path="*" element={<EmptyPage />} />
+//                 </Routes>
+//             </BrowserRouter>
+//         </div>
+//     );
+// }
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import DayList from "./components/DayList";
+import Day from "./components/Day";
+import EmptyPage from "./components/EmptyPage";
+import CreateWord from "./components/CreateWord";
+import CreateDay from "./components/CreateDay";
 
 export default function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Headers/>
-        <Routes>
-            <Route path='/' element={DayList} />
-            <Route path='/day' element={Day} />
-            <Route path='*' element={EmptyPage} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<DayList />} />
+                    <Route path="/day/:day" element={<Day />} />
+                    <Route path="/createWord" element={<CreateWord />} />
+                    <Route path="/createDay" element={<CreateDay />} />
+                    <Route path="*" element={<EmptyPage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,38 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// export default function Header() {
+//     return (
+//         <div className="header">
+//             <h1>
+//                 <Link to="/">토익 영단어(고급)</Link>
+//             </h1>
+//             <div className="menu">
+//                 <a href="#x" className="link">
+//                     단어 추가
+//                 </a>
+//                 <a href="#x" className="link">
+//                     Day 추가
+//                 </a>
+//             </div>
+//         </div>
+//     );
+// }
 import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
-        <div className="Header">
+        <div className="header">
             <h1>
-                <Link to="">토익 영단어(고급)</Link>
+                <Link to="/">토익 영단어(고급)</Link>
             </h1>
             <div className="menu">
-                <Link to="" className="link">
+                <Link to="/CreateWord" className="link">
                     단어 추가
                 </Link>
-                <Link to="" className="link">
-                    Day 추가
+                <Link to="/CreateDay" className="link">
+                    Day 추가 / 삭제
                 </Link>
             </div>
         </div>
